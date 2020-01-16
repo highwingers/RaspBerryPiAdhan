cabf5551018cb8c6d970ad9cd788024c7832829c
saqib khan <saqi19@hotmail.com>
2020-01-16 17:04:23 -0500

    surahs added

--- www/static/common.js
+++ www/static/common.js
@@ -25,25 +25,43 @@ var s = (function () {
         },
         playMedia: function (url, device, mediaUrl, result) {
 
-                $.ajax({
-                    url: url,
-                    dataType: 'text',
-                    method: 'GET',
-                    data: { 'deviceName': device, 'mediaUrl': mediaUrl},
-                    success: function (d) {
-                        result(d);
-                    },
-                    error: function (request, textStatus, errorThrown) {
-                       // alert(textStatus);
-                    },
-                    complete: function (request, textStatus) { //for additional info
-                        //alert(request.responseText);
-                        //alert(textStatus);
-                    }
+            $.ajax({
+                url: url,
+                dataType: 'text',
+                method: 'GET',
+                data: { 'deviceName': device, 'mediaUrl': mediaUrl },
+                success: function (d) {
+                    result(d);
+                },
+                error: function (request, textStatus, errorThrown) {
+                    // alert(textStatus);
+                },
+                complete: function (request, textStatus) { //for additional info
+                    //alert(request.responseText);
+                    //alert(textStatus);
+                }
+            });
+
+        },
+        populateSurahs: function (molvi, selector) {
+
+            url = 'https://server6.mp3quran.net/{molvi}/{surah}.mp3';
+
+            $.getJSON("/static/json/surah.json", function (data) {
+                var items = [];
+                $.each(data, function (key, val) {
+                    _url = url.replace('{molvi}', molvi).replace('{surah}', data[key].index);
+                    $('#' + selector).append('<option value="' + _url + '">' + data[key].title + ' - [Verses ' + data[key].count +'] </option>');
                 });
 
+            });
+
+
+
         }
+
     }
+
 
 
 })();
