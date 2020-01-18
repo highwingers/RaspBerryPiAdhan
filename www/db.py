7c2efd2353628ac60e97c1a05f4f8f4c59d245c4
saqib khan <saqi19@hotmail.com>
2020-01-18 14:25:21 -0500

    db test

---
+++ www/db.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+import sqlite3
+
+conn = sqlite3.connect('/home/pi/RaspBerryPiAdhan/www/data/adhan.db')
+#print "Opened database successfully";
+
+
+conn.execute('''CREATE TABLE COMPANY
+         (ID INT PRIMARY KEY     NOT NULL,
+         NAME           TEXT    NOT NULL,
+         AGE            INT     NOT NULL,
+         ADDRESS        CHAR(50),
+         SALARY         REAL);''')
+
+conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (1, 'Paul', 32, 'California', 20000.00 )");
+
+conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (2, 'Allen', 25, 'Texas', 15000.00 )");
+
+conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (3, 'Teddy', 23, 'Norway', 20000.00 )");
+
+conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 )");
+
+conn.commit()
+print ("Records created successfully")
+
+
+
+
+
+cursor = conn.execute("SELECT id, name, address, salary from COMPANY")
+for row in cursor:
+   print ("NAME = ", row[1], "\n")
+
+
+
+	
+conn.execute('drop table if exists company;')
+
+
+
+
+
+conn.close()
