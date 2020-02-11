29a073070b7ed1a19cf9f24434f1efb0b74327db
saqib khan <saqi19@hotmail.com>
2020-02-11 13:45:17 -0500

    port support

--- www/index.py
+++ www/index.py
@@ -76,7 +76,7 @@ def getSettings():
 @app.route('/api/updateSoftware', methods=['POST'])
 def updateSoftware():
         c = shellcmd().command("sudo sh update.sh")
-        c2 = shellcmd().command("sudo -u pi git pull")
+        c2 = shellcmd().command("sudo git reset --hard && sudo -u pi git pull")
         return jsonify(True)
 
 @app.route('/api/getAdhanSettings')
