74614f9d91ddb6cecb7bcd22b1b4c2d27f6f582d
saqib khan <saqi19@hotmail.com>
2020-02-13 10:15:15 +0500

    methods

--- www/static/common.js
+++ www/static/common.js
@@ -228,7 +228,7 @@ var s = (function () {
                     callback(d);
                 },
                 error: function (jqXHR, exception) {
-                    alert(jqXHR.status)
+                    alert(jqXHR.responseText)
                 }
             });
         }
