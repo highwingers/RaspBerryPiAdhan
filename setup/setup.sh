5b549d8f524e03a3227939286c77bd2f82fb525b
saqib khan <saqi19@hotmail.com>
2020-02-14 23:43:43 +0000

    Button Logic

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,8 +1,5 @@
 chown -R pi:pi  ${PWD%/*}/.git
 
-
-git clone https://github.com/jasbur/RaspiWiFi.git
-
 bash ./flaskserver.service
 
 sh ./dailycron.sh
@@ -21,6 +18,8 @@ pip3 uninstall zeroconf
 pip3 install --timeout 1000 zeroconf==0.24.3
 
 #sh ./apache.sh
+
+sed -i -e '$i \python3 /home/pi/RaspBerryPiAdhan/www/commands/resetbtn.py &\n' /etc/rc.local
 
 
 python3 ${PWD%/*}/www/index.py
---
+++ www/commands/resetbtn.py
@@ -0,0 +1,13 @@
+from gpiozero import Button
+from subprocess import check_call
+from signal import pause
+
+def shutdown():
+    print("Down")
+    with open("/home/pi/RaspBerryPiAdhan/www/commands/test.txt", "a") as myfile:
+        myfile.write("appended text")
+
+shutdown_btn = Button(3, hold_time=2)
+shutdown_btn.when_held = shutdown
+
+pause()
