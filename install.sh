7dd170ea6cec6ecf34f6ff27d509528e3b5e5a6c
Saqib Khan <mrkhan@gmail.com>
2020-02-17 15:55:17 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-apt update
-apt install git
-git clone https://github.com/highwingers/RaspBerryPiAdhan.git
--u pi cp -R RaspBerryPiAdhan orignal_firmware
+#apt update
+#apt install git
+#git clone https://github.com/highwingers/RaspBerryPiAdhan.git
+#chown pi:pi -R RaspBerryPiAdhan
+cp -R RaspBerryPiAdhan orignal_firmware
 cd RaspBerryPiAdhan/setup
-sh ./setup.sh
-
+#sh ./setup.sh
