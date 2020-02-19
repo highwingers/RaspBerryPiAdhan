886105907cf34ea7e93a479f64eea6f2e31237d7
Saqib Khan <mrkhan@gmail.com>
2020-02-19 18:03:20 -0500

    Update resetbtn.py

--- www/commands/resetbtn.py
+++ www/commands/resetbtn.py
@@ -2,34 +2,44 @@ from gpiozero import Button
 from subprocess import check_call
 from signal import pause
 import os
+from pathlib import Path
 import pathlib
 
 
 def reset():
     print("Reseting")
-    currentWork = os.getcwd()
+
+    _file=open("/home/pi/resetlog.txt", "w+")
+
+
+
+    currentWork = str(Path(__file__).parents[3])
+
+    _file.write(currentWork)
+
 
     os.system("sudo rm -r " + currentWork + "/RaspBerryPiAdhan")
     os.system("sudo -u pi cp -R " + currentWork + "/orignal_firmware " + currentWork + "/RaspBerryPiAdhan")
-    #os.system("sudo chown pi:pi -R " + currentWork + "/RaspBerryPiAdhan")
-    #os.system("sudo python3  "+ currentWork +"/RaspBerryPiAdhan/www/index.py")
+
     os.system("rm -f " + currentWork + "/RaspBerryPiAdhan/www/data/adhan.db")
     os.system("sh " + currentWork + "/RaspBerryPiAdhan/setup/db.sh")
-    
+
     print("Reseting Network")
+
+    os.system("sudo shutdown -r +1")
 
     netWorkReset = str((pathlib.Path(__file__).parent)) + '/nmcli.sh'
     os.system('sudo bash '+netWorkReset)
-    os.system("sudo reboot")
+
 
 def shutdown():
     reset()
-    
+
     #with open("/home/pi/RaspBerryPiAdhan/www/commands/test.txt", "a") as myfile:
         #myfile.write("Device Reset.")
 
 
-shutdown_btn = Button(3, hold_time=5)
+shutdown_btn = Button(3, hold_time=3)
 shutdown_btn.when_held = shutdown
 
 pause()
