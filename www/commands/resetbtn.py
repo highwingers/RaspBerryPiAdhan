4217cde260e1cbcebb03b2488f040ab5bf6dc4fe
Saqib Khan <mrkhan@gmail.com>
2020-02-20 15:17:16 -0500

    Update resetbtn.py

--- www/commands/resetbtn.py
+++ www/commands/resetbtn.py
@@ -6,22 +6,21 @@ from pathlib import Path
 import pathlib
 
 
-def reset():
-    print("Reseting")
+currentWork = str(Path(__file__).parents[3])
+_file=open(currentWork + "/resetlog.txt", "a+")
 
-    _file=open("/home/pi/resetlog.txt", "a+")
+def reset_software():
+    
+    print("Reseting Started")    
 
-
-
-    currentWork = str(Path(__file__).parents[3])
-
-    _file.write(currentWork)
+    
+    _file.write(currentWork + "\n")
 
 
     os.system("sudo rm -r " + currentWork + "/RaspBerryPiAdhan")
     os.system("sudo -u pi cp -R " + currentWork + "/orignal_firmware " + currentWork + "/RaspBerryPiAdhan")
     os.system("sudo -u pi crontab -r")
-    _file.write("Done Reseting \n")
+    _file.write("Done Restoring \n")
 
     #os.system("rm -f " + currentWork + "/RaspBerryPiAdhan/www/data/adhan.db")
     #os.system("sh " + currentWork + "/RaspBerryPiAdhan/setup/db.sh")
@@ -29,6 +28,7 @@ def reset():
     #print("Reseting Network")
 
     os.system("sudo shutdown -r +1")
+    _file.write("Shutdown Schedule \n")
 
     #netWorkReset = str((pathlib.Path(__file__).parent)) + '/nmcli.sh'
     #os.system('sudo bash '+netWorkReset)
@@ -40,10 +40,40 @@ def shutdown():
 
     #with open("/home/pi/RaspBerryPiAdhan/www/commands/test.txt", "a") as myfile:
         #myfile.write("Device Reset.")
+held_for=0.0
+def resetInstructions():
+
+    global held_for
+    print("released after", held_for, "seconds.")
+
+    if (held_for > 10.0):
+        print("Restoring Data Files From Orignal Firmware")
+        reset_software()
+        _file.write("** Reset Completed ** \n\n")
+        held_for = 0.0
+    elif (held_for > 5.0):
+        print("Restarting Bluetooth Service after 5 Seconds")
+        os.system("sudo systemctl restart nymea-networkmanager.service")
+        _file.write(" ** Resrtarted Bluetooth Service**  \n\n")
+        held_for = 0.0
 
 
-shutdown_btn = Button(3, hold_time=3)
-shutdown_btn.when_held = shutdown
+    else:
+        print("I'm not going to do anything now …" + str(held_for))
+        held_for = 0.0
+
+def hld():
+        # callback for when button is held
+        #  is called every hold_time seconds
+        global held_for
+        # need to use max() as held_time resets to zero on last callback
+        held_for = max(held_for, reset_btn.held_time + reset_btn.hold_time)
+        print("held for", held_for, "seconds.")
+
+
+reset_btn = Button(3, hold_time=1.0, hold_repeat=True)
+reset_btn.when_held = hld
+reset_btn.when_released  = resetInstructions
 
 pause()
 
