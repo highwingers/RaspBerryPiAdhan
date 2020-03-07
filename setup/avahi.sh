ebf00bb46953a12583dc843f8debedc4da9c682f
saqib khan <saqi19@hotmail.com>
2020-03-07 15:21:57 -0500

    Host Name Logic

--- setup/avahi.sh
+++ setup/avahi.sh
@@ -2,10 +2,13 @@ cd /etc/avahi/services
 
 rm avahi.service
 
+rnd=$(( 1000 + RANDOM % 9999))
+
+echo $rnd
 echo "<?xml version='1.0' standalone='no'?>" >> avahi.service
 echo "<!DOCTYPE service-group SYSTEM 'avahi-service.dtd'>" >> avahi.service
 echo "<service-group>" >> avahi.service
-echo "<name replace-wildcards='yes'>Adhan Player %h</name>" >> avahi.service
+echo "<name replace-wildcards='yes'>Adhan Player ($rnd)</name>" >> avahi.service
 echo "<service>" >> avahi.service
 echo "<type>_http._tcp</type>" >> avahi.service
 echo "<port>5500</port>" >> avahi.service
