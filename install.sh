5fd11f9f0425b59e2399bef0f493284882371b23
saqib khan <saqi19@hotmail.com>
2020-03-08 19:33:20 -0400

    install db on orignal fw

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@ apt install git
 sudo -u pi git clone https://github.com/highwingers/RaspBerryPiAdhan.git
 #chown pi:pi -R RaspBerryPiAdhan
 sudo -u pi cp -R RaspBerryPiAdhan orignal_firmware
+
+sudo sh  orignal_firmware/setup/db.sh
+
+
 #chown pi:pi -R orignal_firmware
 cd RaspBerryPiAdhan/setup
 sh ./setup.sh
