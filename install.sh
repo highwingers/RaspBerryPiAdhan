25743528947d91ff7663d2fc2f2d63c8e0ac2467
Saqib Khan <mrkhan@gmail.com>
2021-07-23 16:57:11 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,17 @@
 apt update
 apt install git -y
 sudo -u pi git clone https://github.com/highwingers/RaspBerryPiAdhan.git
-#chown pi:pi -R RaspBerryPiAdhan
-sudo -u pi cp -R RaspBerryPiAdhan orignal_firmware
 
-sudo rm orignal_firmware/www/data/adhan.db
-cd orignal_firmware/setup
-sudo sh  ./db.sh
-cd ../..
+#chown pi:pi -R RaspBerryPiAdhan
+
+
+#sudo -u pi cp -R RaspBerryPiAdhan orignal_firmware
+#sudo rm orignal_firmware/www/data/adhan.db
+#cd orignal_firmware/setup
+#sudo sh  ./db.sh
+#cd ../..
 
 #chown pi:pi -R orignal_firmware
+
 cd RaspBerryPiAdhan/setup
 sh ./setup.sh
