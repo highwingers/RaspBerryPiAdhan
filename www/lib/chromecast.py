5898f0b54fdf595d78b6446f21512cdae7e7ae02
Saqib Khan <mrkhan@gmail.com>
2021-07-28 23:39:12 -0400

    Update chromecast.py

--- www/lib/chromecast.py
+++ www/lib/chromecast.py
@@ -32,7 +32,7 @@ class chromecast:
         if "youtube.com" in media:
             return self.chromecastPlayYoutube(deviceName, media)
         elif  not media.startswith("http"): 
-            ip = shellcmd().command("ip -o route get to 8.8.8.8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p'")
+            ip = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
             _port =  ':' + utility.ConfigSectionMap("SetUp")["port"]
             media = 'http://'+ ip + _port + media
 
