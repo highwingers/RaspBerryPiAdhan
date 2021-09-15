e8ccd373be964228671f2c5da205eb7f58734dce
Saqib Khan <mrkhan@gmail.com>
2021-09-15 00:29:26 -0400

    Update db.sh

--- setup/db.sh
+++ setup/db.sh
@@ -4,11 +4,11 @@ file=$dir/www/data/adhan.db
 
 python3 $dir/www/lib/sqllite.py False
 
-if [ -f "$file" ]
-then
-	echo "$file found."
-	python3 $dir/www/lib/sqllite.py True
-else
-	python3 $dir/www/lib/sqllite.py False
-fi
+#if [ -f "$file" ]
+#then
+	#echo "$file found."
+	#python3 $dir/www/lib/sqllite.py True
+#else
+	#python3 $dir/www/lib/sqllite.py False
+#fi
 
